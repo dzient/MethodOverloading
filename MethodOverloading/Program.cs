7b8e0375c38a7e5a798a56cb7cff8d331ea3dca4
dzient <34776686+dzient@users.noreply.github.com>
2022-07-07 15:42:28 -0400

    Add files via upload
    Second commit (added comments)

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -12,14 +12,31 @@ namespace MethodOverloading
     
     class Program
     {
+        //Add
+        //Takes two operands and returns sum of the operands
+        // Overloaded for integers
+        //PARAMS: Two integers (a and b)
+        //RETURNS: a+b (int)
         public static int Add(int a, int b)
         {
             return (a + b);
         }
+        //Add
+        //Takes two operands and returns sum of the operands
+        // Overloaded for doubles
+        //PARAMS: Two integers (a and b)
+        //RETURNS: a+b (double)
         public static double Add(double a, double b)
         {
             return (a + b);
         }
+        //Add
+        //Takes two operands and returns sum of the operands
+        //and dollar(s), if needed
+        // Overloaded for strings
+        //PARAMS: Two integers (a and b), boolean value to 
+        //indicate whether dollar(s) should be printed
+        //RETURNS: a+b (string)
         public static string Add(int a, int b, bool dollar)
         {
             string retval = "";
@@ -36,6 +53,7 @@ namespace MethodOverloading
        
         public static void Main(String[] args)
         {
+            // Produce some random #s for testing:
             Random rnd = new Random();
             int s1 = rnd.Next(1000);
             int s2 = rnd.Next(1000);
@@ -45,7 +63,7 @@ namespace MethodOverloading
             double d4 = rnd.Next(1000);
             d1 /= d2;
             d3 /= d4;
-
+            //Print it out:
             Console.WriteLine($"{s1} + {s2} = {Add(s1, s2)} ");
             Console.WriteLine($"{d1} + {d3} = {Add(d1, d3)} ");
             s1 = rnd.Next(25);
